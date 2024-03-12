b397973cfefe8b5a30bd929f63b917973b52470d
sedasdas <qq1820029262@gmail.com>
2024-03-12 20:41:01 +0800

    addlllll

--- cmd/lotus-bench/main.go
+++ cmd/lotus-bench/main.go
@@ -1267,7 +1267,7 @@ var seaCmd = &cli.Command{
 			return xerrors.Errorf("add piece: %w", err)
 		}
 
-		log.Infof("[%d] Running replinmcaltion...", sectorNum)
+		log.Infof("[%d] Running replin mcaltion...", sectorNum)
 
 		if err != nil {
 			return err
